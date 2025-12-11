47aa5ed931f2128593631bf70ebf0e889a0cf11b
Anupong Doungsimma <anupong.do@ku.th>
2025-12-11 15:37:18 +0700

    internet lab1

---
+++ lab1/operators.php
@@ -0,0 +1,65 @@
+<?php
+
+$tod = 10;
+
+$taohua = 20;
+
+// calculations
+
+$add = $tod + $taohua;
+
+$sub = $tod - $taohua;
+
+$mul = $tod * $taohua;
+
+$div = $tod / $taohua;
+
+// Show given values
+
+echo "Given: <br>";
+
+echo "tod = $tod <br>";
+
+echo "taohua = $taohua <br><br>";
+
+// Addition
+
+echo "<strong>Addition:</strong> <br>";
+
+echo "z = tod + taohua <br>";
+
+echo "z = $tod + $taohua <br>";
+
+echo "Result: z = $add <br><br>";
+
+// Subtraction
+
+echo "<strong>Subtraction:</strong> <br>";
+
+echo "z = tod - taohua <br>";
+
+echo "z = $tod - $taohua <br>";
+
+echo "Result: z = $sub <br><br>";
+
+// Multiplication
+
+echo "<strong>Multiplication:</strong> <br>";
+
+echo "z = tod * taohua <br>";
+
+echo "z = $tod * $taohua <br>";
+
+echo "Result: z = $mul <br><br>";
+
+// Division
+
+echo "<strong>Division:</strong> <br>";
+
+echo "z = tod / taohua <br>";
+
+echo "z = $tod / $taohua <br>";
+
+echo "Result: z = $div <br><br>";
+
+?>
